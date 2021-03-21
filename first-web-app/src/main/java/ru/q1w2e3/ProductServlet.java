6b73c6f0e7b12b3259c9d27574b2ae19be8dbe46
Yuliya Yuldasheva <julia.vertegel@gmail.com>
2021-03-21 22:40:10 +0700

    Lesson_2_HW

--- first-web-app/src/main/java/ru/q1w2e3/ProductServlet.java
+++ first-web-app/src/main/java/ru/q1w2e3/ProductServlet.java
@@ -11,24 +11,25 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-@WebServlet(urlPatterns = "/product")
+@WebServlet(urlPatterns = "/product/*")
 public class ProductServlet extends HttpServlet {
 
-    private static final Logger logger = LoggerFactory.getLogger(ProductServlet.class);
+    private static final Pattern pathParam = Pattern.compile("\\/(\\d*)$");
 
     private ProductRepository productRepository;
 
     @Override
     public void init() throws ServletException {
-        logger.info("Product servlet");
-
         productRepository = (ProductRepository) getServletContext().getAttribute("productRepository");
     }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        if (req.getPathInfo() == null || req.getPathInfo().equals("") || req.getPathInfo().equals("/")) {
             resp.getWriter().println("<table>");
             resp.getWriter().println("<tr>");
             resp.getWriter().println("<th>Id</th>");
@@ -46,6 +47,24 @@ public class ProductServlet extends HttpServlet {
                 resp.getWriter().println("</tr>");
             }
             resp.getWriter().println("</table>");
-
+        } else {
+            Matcher matcher = pathParam.matcher(req.getPathInfo());
+            if (matcher.matches()) {
+                long id;
+                try {
+                    id = Long.parseLong(matcher.group(1));
+                } catch (NumberFormatException ex) {
+                    resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+                    return;
+                }
+                Product product = productRepository.findById(id);
+                resp.getWriter().println("<p><b>Product info</b></p>");
+                resp.getWriter().println("Name: " + product.getName() + "<br>");
+                resp.getWriter().println("Description: " + product.getDescription() + "<br>");
+                resp.getWriter().println("Price: " + product.getPrice() + "<br>");
+                return;
+            }
+            resp.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
     }
 }
